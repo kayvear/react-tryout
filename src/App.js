56be11d0d3761236949a58db6d1ef8b8831d0bb1
Krish <krish@MacPro.local>
2021-01-19 11:08:23 -0500

    Using states and effects

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import toolico from './logo.svg';
-
-function Header(props) {
-  // console.log(props);
-  return (
-    <header>
-      <h1>{props.name}'s Workshop</h1>
-    </header>
-  )
-};
-
-function Main(props) {
-  return (
-    <section>
-      <p>Tools to {props.verb} all your problems</p>
-      <img src={toolico} height={200} alt="fake tool icon"/>
-      <ul style={{ textAlign: "left" }}>
-      {props.tools.map((tool) => (
-        <li key={tool.id}>{tool.name}</li>
-      ))}
-      </ul>
-    </section>
-  )
-};
-
-
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  )
-};
-
-const tools = [
-  "Hammer",
-  "Spanner",
-  "Wrench",
-  "Hacksaw"
-];
-
-const toolObjects = tools.map((tool, i) => ({id: i, name: tool}));
-// console.log(toolObjects);
-
 
 
 function App() {
+  const [level, setLevel] = useState('beginner');
+
+  useEffect( () => {
+    console.log(`Expertise level is ${level}...`); 
+  }, [level]); // tracks the dependency of this variable and only logs when value changes
+
   return (
-    <div className="App">
-       < Header name="Krish"/>
-       < Main verb="fix" tools={toolObjects}/>
-       < Footer year={new Date().getFullYear()}/>
-    </div>
+    <>
+      <h1>Current expertise level is {level} :) </h1>
+      <button onClick= {() => setLevel('beginner')}>
+      Begin
+      </button>
+      <button onClick= {() => setLevel('intermediate')}>
+      Intermediate
+      </button>
+      <button onClick= {() => setLevel('expert')}>
+      Expert
+      </button>
+    
+    
+    </>
   );
 }
 
