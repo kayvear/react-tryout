3f62801edd9ccc707009ee9420b953df1470d80a
Krish <krish@MacPro.local>
2021-01-18 20:12:28 -0500

    Added template/dynamic content

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 import './App.css';
+import toolico from './logo.svg';
 
-function Header() {
+function Header(props) {
+  // console.log(props);
   return (
     <header>
-      <h1>Krish's Workshop</h1>
+      <h1>{props.name}'s Workshop</h1>
     </header>
   )
-}
+};
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>Tools to fix all your problems</p>
+      <p>Tools to {props.verb} all your problems</p>
+      <img src={toolico} height={200} alt="fake tool icon"/>
+      <ul style={{ textAlign: "left" }}>
+      {props.tools.map((tool) => (
+        <li key={tool.id}>{tool.name}</li>
+      ))}
+      </ul>
     </section>
   )
-}
+};
 
-function Footer() {
+
+function Footer(props) {
   return (
     <footer>
-      <p>Here to help!</p>
+      <p>Copyright {props.year}</p>
     </footer>
   )
-}
+};
+
+const tools = [
+  "Hammer",
+  "Spanner",
+  "Wrench",
+  "Hacksaw"
+];
+
+const toolObjects = tools.map((tool, i) => ({id: i, name: tool}));
+// console.log(toolObjects);
+
+
 
 function App() {
   return (
     <div className="App">
-       < Header />
-       < Main />
-       < Footer />
+       < Header name="Krish"/>
+       < Main verb="fix" tools={toolObjects}/>
+       < Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
