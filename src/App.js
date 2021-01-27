a340549c37cfc3f9ace7ebe1c3eff39ac4d97770
Krish <krish@MacPro.local>
2021-01-27 18:01:34 -0500

    fetching from an api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+// https://api.github.com/users/kayvear
 
-function App() {
-  const [checked, setChecked] = useState(false);
+function App( { login }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  function toggle() {
-    setChecked((checked) => !checked);
-  }
-  
   useEffect( () => {
-    console.log(`Value of checkbox is ${checked}`)
-  }, [checked]);
+    if (!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+    .then((response) => response.json())
+    .then(setData)
+    .then( () => setLoading(false))
+    .catch(setError);
+  }, [login]);
 
+  if (loading) return <h1>Loading Page...</h1>;
+  if (error)
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
+
+  //console.log(data);
   return (
-    <>
-      <input type="checkbox" 
-      value ={checked} 
-      onChange = {toggle} 
-      />
-      <p>{checked ? "checked" : 'not checked'}</p>
-    </>
+    <div>
+    <h1>{data.name}</h1>
+    <p>{data.login}</p>
+    <img alt={data.login} src={data.avatar_url} />
+    </div>
   );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 
 ReactDOM.render(
-    <App />,
+    <App login='kayvear'/>,
   document.getElementById('root')
 );
 
